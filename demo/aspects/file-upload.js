ed5ef75f15a472e57bd5bb6292aa70adcdc75f8c
west270 <bjwest31@outlook.com>
2021-11-17 15:08:07 -0500

    bg-issue #1165 adds support for byte type

--- demo/aspects/file-upload.js
+++ demo/aspects/file-upload.js
@@ -16,7 +16,20 @@ var fileUpload = {
         maxSize: '5242880',
         title: 'Image file'
       }
-    }
+    },
+    'bytes_key_optional': {
+      title: 'Bytes Type Optional',
+      type: 'file',
+      format: 'bytes',
+      maxSize: '5242880'
+    },
+    'bytes_key': {
+      title: 'Bytes Type',
+      type: 'file',
+      format: 'bytes',
+      maxSize: '5242880',
+      required: true
+    },
   },
   form: [
     {
@@ -30,7 +43,20 @@ var fileUpload = {
       title: 'Image upload',
       description: 'Upload a file',
       placeholder: 'Click here or drop files to upload'
-    }
+    },
+    {
+      key: 'bytes_key_optional',
+      title: 'Bytes type optional',
+      description: 'Upload a file',
+      placeholder: 'Click here or drop files to upload'
+    },
+    {
+      key: 'bytes_key',
+      title: 'Bytes type',
+      description: 'Upload a file',
+      placeholder: 'Click here or drop files to upload'
+    },
+
   ]
 };
 
