6cbd483a1e536256ed56e692272990e2b87100d7
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2021-11-23 14:55:34 -0500

    Code formatting

--- src/directives/datetime.js
+++ src/directives/datetime.js
@@ -1,42 +1,43 @@
-
 // This is heavily based on the 'eonasdan-bootstrap-datetimepicker' package
 
-import jquery from 'jquery';
-import moment from 'moment';
+import jquery from "jquery";
+import moment from "moment";
 
-dateTimeDirective.$inject = ['$timeout'];
+dateTimeDirective.$inject = ["$timeout"];
 export function dateTimeDirective($timeout) {
   return {
-    restrict: 'EA',
-    require: 'ngModel',
+    restrict: "EA",
+    require: "ngModel",
     scope: false,
     link: function ($scope, $element, $attrs, ngModel) {
-      var dpElement = $element.parent().hasClass('input-group') ? $element.parent() : $element;
+      var dpElement = $element.parent().hasClass("input-group")
+        ? $element.parent()
+        : $element;
 
       // Set up some default options that can be overridden
       var options = Object.assign(
         {
-          format: 'MM/DD/YYYY hh:mm A',
+          format: "MM/DD/YYYY hh:mm A",
           keepInvalid: true,
           showTodayButton: true,
-          showClear: true
+          showClear: true,
         },
         $scope.form.options
       );
 
       // This thing is probably expensive - make sure we destroy it
-      $scope.$on('$destroy', function() {
-        dpElement.data('DateTimePicker').destroy();
+      $scope.$on("$destroy", function () {
+        dpElement.data("DateTimePicker").destroy();
       });
 
-      ngModel.$parsers.unshift(function(value) {
+      ngModel.$parsers.unshift(function (value) {
         // We need to translate when the text field is manually changed
-        if(typeof value === 'string') {
-          if(value.trim().length === 0) {
+        if (typeof value === "string") {
+          if (value.trim().length === 0) {
             return null;
           }
 
-          var parsedMoment = moment(value, options['format']);
+          var parsedMoment = moment(value, options["format"]);
           return parsedMoment.isValid() ? parsedMoment.valueOf() : value;
         }
         return value;
@@ -44,13 +45,15 @@ export function dateTimeDirective($timeout) {
 
       ngModel.$render = function () {
         // Make sure we clear the datepicker if there's no value
-        if(!ngModel.$viewValue && dpElement.data('DateTimePicker').date()) {
-          dpElement.data('DateTimePicker').clear();
+        if (!ngModel.$viewValue && dpElement.data("DateTimePicker").date()) {
+          dpElement.data("DateTimePicker").clear();
         }
         // We have a value, we just need to give the datepicker a moment object
         else if (ngModel.$viewValue) {
-          var viewMoment = moment.isMoment(ngModel.$viewValue) ? ngModel.$viewValue : moment(ngModel.$viewValue, 'x');
-          dpElement.data('DateTimePicker').date(viewMoment);
+          var viewMoment = moment.isMoment(ngModel.$viewValue)
+            ? ngModel.$viewValue
+            : moment(ngModel.$viewValue, "x");
+          dpElement.data("DateTimePicker").date(viewMoment);
         }
       };
 
@@ -58,15 +61,16 @@ export function dateTimeDirective($timeout) {
       // Date will be false if the date has been cleared, which we want to translate to null
       // Data.valueOf() will be NaN if the control couldn't parse the input (which means it's probably a string),
       // which we want to leave unchanged
-      dpElement.on('dp.change', function (changeEvent) {
-        var newValue = changeEvent.date === false ? null : changeEvent.date.valueOf();
+      dpElement.on("dp.change", function (changeEvent) {
+        var newValue =
+          changeEvent.date === false ? null : changeEvent.date.valueOf();
 
-        if(!Number.isNaN(newValue)) {
+        if (!Number.isNaN(newValue)) {
           ngModel.$setViewValue(newValue);
         }
       });
 
       dpElement.datetimepicker(options);
-    }
+    },
   };
 }
--- src/directives/dictionary.js
+++ src/directives/dictionary.js
@@ -1,50 +1,55 @@
-
-import angular from 'angular';
+import angular from "angular";
 
 export function dictionaryDirective() {
   return {
-    restrict: 'A',
-    require: 'ngModel',
+    restrict: "A",
+    require: "ngModel",
     scope: true,
     priority: 500,
-    link: function(scope, element, attrs, ngModel) {
-
+    link: function (scope, element, attrs, ngModel) {
       // We need the ngModelController in several places, most notably for errors. So we emit it up to the
       // decorator directive so it can be put on the right scope.
       // Basically, stuff doesn't work inside sfArray without this
-      scope.$emit('schemaFormPropagateNgModelController', ngModel);
+      scope.$emit("schemaFormPropagateNgModelController", ngModel);
 
       // Make sure that the value is stringified so it can be displayed correctly
-      ngModel.$formatters.push(function(value) {
+      ngModel.$formatters.push(function (value) {
         return angular.isObject(value) ? angular.toJson(value) : value;
       });
 
-      ngModel.$parsers.unshift(function(value) {
-        if (value === null || value === undefined || value.trim().length === 0) return null;
+      ngModel.$parsers.unshift(function (value) {
+        if (value === null || value === undefined || value.trim().length === 0)
+          return null;
 
         value = value.trim();
 
-        if(value.indexOf('{') === 0) {
+        if (value.indexOf("{") === 0) {
           try {
             return JSON.parse(value);
-          } catch(e) { }
+          } catch (e) {}
         }
 
         return value;
       });
 
-      ngModel.$validators['tv4-302'] = function(modelValue, viewValue) {
-        return !scope.form.required || ngModel.$pristine || (modelValue !== null && modelValue !== undefined);
+      ngModel.$validators["tv4-302"] = function (modelValue, viewValue) {
+        return (
+          !scope.form.required ||
+          ngModel.$pristine ||
+          (modelValue !== null && modelValue !== undefined)
+        );
       };
-      ngModel.$validators['badObject'] = function(modelValue, viewValue) {
-        return (typeof modelValue === 'object') || (typeof modelValue === 'undefined');
+      ngModel.$validators["badObject"] = function (modelValue, viewValue) {
+        return (
+          typeof modelValue === "object" || typeof modelValue === "undefined"
+        );
       };
 
       // Need to listen for the validate event
-      scope.$on('schemaFormValidate', function() {
+      scope.$on("schemaFormValidate", function () {
         ngModel.$setDirty();
         ngModel.$validate();
       });
-    }
+    },
   };
 }
