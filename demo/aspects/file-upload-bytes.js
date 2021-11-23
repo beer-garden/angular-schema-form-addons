f0b99dad21aab09e982a9f736ba35b27109c38c5
west270 <bjwest31@outlook.com>
2021-11-23 13:37:40 -0500

    bg-issue #1165 changed styling and location of bytes in addons test

--- demo/aspects/file-upload-bytes.js
+++ demo/aspects/file-upload-bytes.js
@@ -1,36 +1,34 @@
 
 var fileUpload = {
   schema: {
-    'image': {
-      title: 'Image file',
+    'bytes_key_optional': {
+      title: 'Bytes Type Optional',
       type: 'file',
-      format: 'base64',
+      format: 'bytes',
       maxSize: '5242880'
     },
-    'images': {
-      type: 'array',
-      maxItems: 2,
-      items: {
-        type: 'file',
-        format: 'base64',
-        maxSize: '5242880',
-        title: 'Image file'
-      }
-    }
+    'bytes_key': {
+      title: 'Bytes Type',
+      type: 'file',
+      format: 'bytes',
+      maxSize: '5242880',
+      required: true
+    },
   },
   form: [
     {
-      key: 'images',
-      title: 'Images upload',
-      description: 'what',
+      key: 'bytes_key_optional',
+      title: 'Bytes type optional',
+      description: 'Upload a file',
       placeholder: 'Click here or drop files to upload'
     },
     {
-      key: 'image',
-      title: 'Image upload',
+      key: 'bytes_key',
+      title: 'Bytes type',
       description: 'Upload a file',
       placeholder: 'Click here or drop files to upload'
-    }
+    },
+
   ]
 };
 
--- src/directives/file-upload-bytes.js
+++ src/directives/file-upload-bytes.js
@@ -1,42 +1,52 @@
 fileUploadBytesDirective.$inject = ['$rootScope'];
+
 function fileUploadBytesDirective($rootScope) {
-  return {
-    restrict: 'A',
-    require: 'ngModel',
-    scope: true,
-    link: function(scope, element, attrs, ngModel) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        scope: true,
+        link: function (scope, element, attrs, ngModel) {
 
-      scope.ngModel = ngModel;
-      scope.key = ngModel.$$scope.getKey()[ngModel.$$scope.getKey().length-1];
-      scope.fileInput = element[0].querySelector('.file-upload-field');
-      scope.inputText = "No File Chosen";
-      scope.fileName = null
+            scope.ngModel = ngModel;
+            scope.key = ngModel.$$scope.getKey()[ngModel.$$scope.getKey().length - 1];
+            scope.fileInput = element[0].querySelector('.file-upload-field');
+            scope.fileName = null;
+            scope.hasFile = false;
 
-      scope.selectedFile = function () {
-        let file = document.getElementById(scope.key).files[0];
-        if (file) {
-          scope.fileName = file.name;
-        } else {
-          scope.fileName = null;
+            scope.selectedFile = function () {
+                let file = document.getElementById(scope.key).files[0];
+                if (file) {
+                    scope.fileName = file.name;
+                    scope.hasFile = true;
+                } else {
+                    scope.fileName = null;
+                    scope.hasFile = false;
+                }
+                scope.validateValue();
+                scope.$apply();
+            }
+
+            scope.removeFile = function () {
+                console.log("testing remove")
+                document.getElementById(scope.key).value = "";
+                scope.hasFile = false;
+                scope.fileName = null;
+                scope.validateValue();
+            }
+
+            scope.validateValue = function () {
+                if (!scope.hasFile && ngModel.$$scope.form.required && !ngModel.$modelValue) {
+                    ngModel.$setValidity('tv4-302', false);
+                } else {
+                    ngModel.$setValidity('tv4-302', true);
+                }
+            }
+
+            scope.$on('schemaFormValidate', function () {
+                scope.validateValue();
+            })
         }
-        scope.validateValue();
-        scope.$apply();
-      }
-
-      scope.validateValue = function () {
-        Object.keys(ngModel.$error).forEach(function(k) {
-          ngModel.$setValidity(k, true);
-        });
-        if (!document.getElementById(scope.key).files[0] && ngModel.$$scope.form.required && !ngModel.$modelValue) {
-          ngModel.$setValidity('tv4-302', false);
-        }
-      }
-      
-      scope.$on('schemaFormValidate', function() {
-        scope.validateValue();
-      })
     }
-  }
 }
 
-export { fileUploadBytesDirective };
+export {fileUploadBytesDirective};
